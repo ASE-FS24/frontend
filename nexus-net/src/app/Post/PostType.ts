2ab03562d79820664cf9bf3992d28dee3c645967
Danielgergely <daniel.gergely@fischerspindle.com>
2024-03-11 12:11:32 +0100

    SCRUM-11 feature(post): updated post layout

--- nexus-net/src/app/Post/PostType.ts
+++ nexus-net/src/app/Post/PostType.ts
@@ -3,11 +3,12 @@ import {Comment} from "../Comment/CommentType";
 export interface Post {
     id: string;
     authorId: string;
+    type: string;
     title: string;
     shortDescription?: string;
     description: string;
     image?: string;
     comments: Array<Comment>
     likes: number;
-    Hashtags: Array<string>
+    hashtags: Array<string>
 }
