d084266682eee695f034060629f781195695ac46
Daniel Gergely <daniel.gergely@uzh.ch>
2024-03-16 16:57:46 +0100

    SCRUM-19 feature(post): author, creation date & type added to post view

--- nexus-net/src/app/Post/PostService.ts
+++ nexus-net/src/app/Post/PostService.ts
@@ -5,24 +5,30 @@ const baseurl = process.env.REACT_APP_POST_BASEURL;
 const mockPosts = [
     {
         id: "MockPost101",
-        authorId: "MockUser1",
+        author: "mockuser1",
         type: "Project",
         title: "Mock Title 1",
         shortDescription: "This is a short description of the first mock post.",
         description: "This is the long description of the first mock post. This is the long description of the first mock post. This is the long description of the first mock post. This is the long description of the first mock post.",
         image: "",
+        createdDate: "2024-03-12T12:00:00Z",
+        edited: false,
+        editedDate: "",
         comments: [],
         likes: 4,
         hashtags: ["#MockPost4ever"]
     },
     {
         id: "MockPost102",
-        authorId: "MockUser1",
-        type: "Project",
+        author: "mockuser1",
+        type: "Post",
         title: "Mock Title 2",
         shortDescription: "This is a short description of the second mock post.",
         description: "This is the long description of the second mock post. This is the long description of the second mock post. This is the long description of the second mock post. This is the long description of the second mock post.",
         image: "",
+        createdDate: "2024-03-13T16:27:00Z",
+        edited: true,
+        editedDate: "2024-03-14T11:19:00Z",
         comments: [],
         likes: 3,
         hashtags: ["#MockPost4ever"]
--- nexus-net/src/app/Post/PostType.ts
+++ nexus-net/src/app/Post/PostType.ts
@@ -2,12 +2,15 @@ import {Comment} from "../Comment/CommentType";
 
 export interface Post {
     id: string;
-    authorId: string;
+    author: string;
     type: string;
     title: string;
     shortDescription?: string;
     description: string;
     image?: string;
+    createdDate: string;
+    edited: boolean;
+    editedDate: string;
     comments: Array<Comment>
     likes: number;
     hashtags: Array<string>
