c0294b7240610aedb8b46a25c024930943956da1
Danielgergely <daniel.gergely@fischerspindle.com>
2024-03-16 16:57:46 +0100

    SCRUM-19 feature(post): author, creation date & type added to post view

--- nexus-net/src/app/Post/PostComponent.tsx
+++ nexus-net/src/app/Post/PostComponent.tsx
@@ -4,6 +4,9 @@ import {ReactComponent as LikeSVG} from "../../static/images/heart.svg";
 import {ReactComponent as FireSVG} from "../../static/images/fire.svg";
 import {ReactComponent as ShareSVG} from "../../static/images/share.svg";
 import {ReactComponent as ReportSVG} from "../../static/images/report.svg";
+import {ReactComponent as ProjectSVG} from "../../static/images/project.svg";
+import {ReactComponent as PostSVG} from "../../static/images/camera.svg";
+import {dateFormatter} from "../Util/util";
 
 
 const StyledPost = styled.div`
@@ -17,9 +20,29 @@ const StyledPost = styled.div`
 
 const StyledPostContent = styled.div`
   display: flex;
+  width: 100%;
   justify-content: center;
   flex-direction: column;
   padding: 25px;
+`;
+
+const StyledPostHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: -10px 0 10px 0;
+  font-style: italic;
+`;
+
+const StyledPostType = styled.div<{color: string}>`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  color: ${props => props.color};
+`;
+
+const StyledPostAuthor = styled.div`
+  width: fit-content;
+  margin-left: auto;
 `;
 
 const StyledPostTitle = styled.div`
@@ -61,7 +84,7 @@ const StyledLikes = styled.div`
   top: 8px;
   right: 30px;
   font-size: 1.5rem;
-  
+
   &:hover {
     cursor: pointer;
     scale: 1.025;
@@ -69,9 +92,19 @@ const StyledLikes = styled.div`
 `;
 
 function PostComponent({post}: { post: Post }) {
+    const postDate = dateFormatter(post.edited ? post.editedDate : post.createdDate);
     return (
         <StyledPost>
             <StyledPostContent>
+                <StyledPostHeader>
+                    <StyledPostType color={post.type === "Post" ? "#00aaff": "#ff0000"}>
+                        {post.type === "Post" ?
+                            <PostSVG style={{width: "30px", height: "30px", color: "#00aaff"}}/> :
+                            <ProjectSVG style={{width: "30px", height: "30px", color: "#ff0000"}}/>}
+                        {post.type}
+                    </StyledPostType>
+                        <StyledPostAuthor>{post.author} - {postDate}</StyledPostAuthor>
+                </StyledPostHeader>
                 <StyledPostTitle>{post.title}</StyledPostTitle>
                 <StyledPostDescription>{post.description}</StyledPostDescription>
             </StyledPostContent>
